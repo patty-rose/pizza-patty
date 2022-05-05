51051b786e5f2e2a7706faee12e6c8b8aaf39dee
patty-rose <oteropatty@gmail.com>
2022-05-05 14:25:33 -0700

    WIP calculatePizzaCost test

--- js/scripts.js
+++ js/scripts.js
@@ -5,12 +5,24 @@ function Pizza(toppings){
   this.pizzaCost = 0;
 }
 
-// Pizza.prototype.calculatePizzaCost()
+Pizza.prototype.calculatePizzaCost(pizza){
+
+}
 
 // //Order object and methods
 // Order()
 
 // Order.prototype.calculateOrderCost()
+const sizeCost = [[small, 5], [medium, 10] ,[large, 15], [extraLarge, 20]];
+const toppingsCost = [[anchovies, 2], [pepperoni, 2], [sausage, 2], ]
+Small : $5
+Medium : $10
+Large : $15
+Extra Large : $20
+
+Toppings:
+Meat: $2 per topping
+Veggie: $1 per topping
 
 
 //User Interface Logic
