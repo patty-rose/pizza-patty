96082adf0db3b3d8d17ae503a01c3d0141e82dab
patty-rose <oteropatty@gmail.com>
2022-05-07 22:09:21 -0700

    WIP add scripts to display order and cost

--- js/scripts.js
+++ js/scripts.js
@@ -1,30 +1,4 @@
 //Business Logic
-//Pizza object and methods
-function Pizza(toppings, size){
-  this.toppings = toppings;
-  this.size = size;
-  this.pizzaCost = 0;
-}  
-
-Pizza.prototype.calculatePizzaCost = function(){
-  this.pizzaCost = 0
-  this.pizzaCost = calculateSizeCost(this.size) + calculateToppingsCost(this.toppings);
-  return this.pizzaCost;
-};
-
-// //Order object and methods
-function Order(pizzas){
-  this.pizzas = pizzas;
-  this.orderCost = 0;
-}
-
-Order.prototype.calculateOrderCost = function(){
-  this.orderCost = 0;
-  for (let i=0; i < this.pizzas.length; i++) {
-    this.orderCost += this.pizzas[i].calculatePizzaCost();
-  } return this.orderCost
-};
-
 function calculateToppingsCost(toppings){
   let toppingsCost = 0;
   const toppingsCostMap = {
@@ -58,22 +32,61 @@ function calculateSizeCost(size) {
   return sizeCostMap[size];
 }
 
-//User Interface Logic
-let currentOrder = new Order;
+//Pizza object and methods
+function Pizza(toppings, size){
+  this.toppings = toppings;
+  this.size = size;
+  this.pizzaCost = 0;
+}  
+
+Pizza.prototype.calculatePizzaCost = function(){
+  this.pizzaCost = 0
+  this.pizzaCost = calculateSizeCost(this.size) + calculateToppingsCost(this.toppings);
+  return this.pizzaCost;
+};
+
+// //Order object and methods
+function Order(pizzas){
+  this.pizzas = pizzas;
+  this.orderCost = 0;
+}
+
+Order.prototype.calculateOrderCost = function(){
+  this.orderCost = 0;
+  for (let i=0; i < this.pizzas.length; i++) {
+    this.orderCost += this.pizzas[i].calculatePizzaCost();
+  } return this.orderCost
+};
+
+// User Interface Logic
+
+function showPizzas(pizzas) {
+  let displayPizzasHTML = ("");
+  //loop through pizzas object
+  //let pizzaSizeDisplay = key[size];
+  //let pizza toppingsDisplay = key[toppings].join(", ");
+
+}
+
 let pizzasOnCurrentOrder = [];
+let currentOrder = new Order(pizzasOnCurrentOrder);
 
 $(document).ready(function() {
   $("form#order").submit(function(event) {
     event.preventDefault();
     const pizzaSizeSubmitted = $("#size").val();
     let toppingsSubmitted = [];
-
     $('input[name="toppings"]:checked').each(function() {
       toppingsSubmitted.push(this.id);
     });
 
+
     const newPizza = new Pizza(toppingsSubmitted, pizzaSizeSubmitted);
     newPizza.calculatePizzaCost();
     pizzasOnCurrentOrder.push(newPizza);
+
+    $(".displayOrder").text(pizzasOnCurrentOrder);
+    $(".displayOrderCost").text(currentOrder.calculateOrderCost());
+    $( 'input[type="checkbox"]' ).prop('checked', false);
   })
 })
